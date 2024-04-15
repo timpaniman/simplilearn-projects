c2ebc08c802a41ef295e4a45db7435020d3b0099
Kevin Lee <timpaniman@gmail.com>
2024-04-15 16:20:16 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,8 +73,14 @@ resource "aws_instance" "ubuntu" {
     inline = [
       "sudo apt update && upgrade",
       "sudo apt install -y python3.8",
-      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ >  /etc/apt/sources.list.d/jenkins.list'",
+#      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
+#     "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ >  /etc/apt/sources.list.d/jenkins.list'",
+      "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null"
+      "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null"
+
       "sudo apt-get update",
       "sudo apt-get install -y openjdk-8-jre",
       "sudo apt-get install -y jenkins",
