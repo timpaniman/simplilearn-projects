83a04c744c4ed558a8332a36a14fb2e860de243b
Kevin Lee <timpaniman@gmail.com>
2024-04-15 15:26:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
 
 resource "aws_security_group" "allow_SSH" {
   name        = "allow_SSH"
