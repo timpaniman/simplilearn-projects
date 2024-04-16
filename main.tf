bb5b2c9970c4d91710000d2982c70c22a061490f
Kevin Lee <timpaniman@gmail.com>
2024-04-15 17:18:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,11 +73,8 @@ resource "aws_instance" "ubuntu" {
     inline = [
       "sudo apt update && upgrade",
       "sudo apt install -y python3.8",
-      "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \n
-       https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
-      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \n
-       https://pkg.jenkins.io/debian-stable binary/ | sudo tee \n
-       /etc/apt/sources.list.d/jenkins.list > /dev/null",
+      "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
+      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt-get update",
       "sudo apt-get install -y fontconfig openjdk-17-jre",
       "sudo apt-get install -y jenkins",
