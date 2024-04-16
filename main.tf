6bcae93b6aa5ce1070f760fb1ef5915f61173f19
Kevin Lee <timpaniman@gmail.com>
2024-04-15 17:00:52 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -73,15 +73,13 @@ resource "aws_instance" "ubuntu" {
     inline = [
       "sudo apt update && upgrade",
       "sudo apt install -y python3.8",
-#      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-#     "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ >  /etc/apt/sources.list.d/jenkins.list'",
-      "sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-       /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-      "sudo echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+      "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+       https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
+      "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
        https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
        /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt-get update",
-      "sudo apt-get install -y openjdk-8-jre",
+      "sudo apt-get install -y fontconfig openjdk-17-jre",
       "sudo apt-get install -y jenkins",
       "sudo apt-get install -y docker docker.io",
       "sudo chmod 777 /var/run/docker.sock",
